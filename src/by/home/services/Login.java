af5444d9621507c268e3ecb52023462dd2210173
Максим <maxum4uk@tut.by>
2019-10-14 01:46:11 +0300

    мапанг

--- src/by/home/services/Login.java
+++ src/by/home/services/Login.java
@@ -8,56 +8,72 @@ public class Login {
 
         boolean rezultBigLetter = Login.checkBigLetter(login);
         boolean rezultNumeral = Login.checkNumeral(login);
-        boolean rezultSummLetter = Login.checkSummLetter(login);
+        boolean rezultSummLetter = Login.checkCountLetter(login);
         boolean rezultOther = Login.checkOther(login);
 
-        if(rezultBigLetter == true && rezultNumeral == true && rezultSummLetter == true && rezultOther == true){
+        if(rezultBigLetter && rezultNumeral && rezultSummLetter && rezultOther){
             rezult = true;
         }
 
         return rezult;
     }
 
-
+    /**
+     * Разбивает строку и ищет в ней заглавную букву.
+     *
+     * @param login строка.
+     * @return результат проверки есть ли в троке заглавная буква
+     */
     public static boolean checkBigLetter(String login) {
         boolean bol = false;
         for (int i = login.length() - 1; i >= 0; i--) {
             if (Character.isUpperCase(login.charAt(i))) {
                 bol = true;
+                break;
             }
         }
         return bol;
     }
-
+    /**
+     * Разбивает строку и ищет в ней цифры.
+     *
+     * @param login строка.
+     * @return результат проверки есть ли в троке цыфры
+     */
     public static boolean checkNumeral(String login) {
         boolean bol1 = false;
-        char[] letter = login.toCharArray();
-        for (int i = 0; i < letter.length; i++) {
-            int x = (int) letter[i];
-            System.out.println(x);
-            if (x < 58 && x > 47) {
+        for (int i = login.length() - 1; i >= 0; i--) {
+            if (Character.isDigit(login.charAt(i))) {
                 bol1 = true;
+                break;
             }
         }
         return bol1;
     }
-
-    public static boolean checkSummLetter(String login) {
+    /**
+     * Находит длину строки.
+     *
+     * @param login строка.
+     * @return результат проверки  состоит ли строка от 6 до 16 символов
+     */
+    public static boolean checkCountLetter(String login) {
         boolean bol2 = false;
         if (login.length() < 17 && login.length() > 5) {
             bol2 = true;
         }
         return bol2;
     }
-
+    /**
+     * Проверяет строку на условие
+     * в строке могут быть только буквы, цифры, знак нижнего подчёркивания, тире
+     *
+     * @param login строка.
+     * @return результат проверки
+     */
     public static boolean checkOther(String login) {
         boolean bol3 = false;
-        char[] letter = login.toCharArray();
-        for (int i = 0; i < letter.length; i++) {
-            int x = (int) letter[i];
-            if (x == 45 || x == 95 || (x < 91 && x > 64) || (x < 123 && x > 96) || (x < 47 && x > 58)) {
-                bol3 = true;
-            }
+        if (login.matches("[a-zA-Z0-9_\\-]*")) {
+            bol3 = true;
         }
         return bol3;
     }
